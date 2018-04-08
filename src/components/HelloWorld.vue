c32628d95df754876bc0a9f5b19ac70d145af0ef
vuetz <414065031@qq.com>
2018-04-08 18:11:37 +0800

    使用cnpm install iview --save  安装iview  同时在main.js中引入iview 以及  css  以及 vue.use(view)

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,84 @@
+<template>
+<div>
+ <div class="layout">
+    <Layout>
+        <Header>Header</Header>
+        <Content>Content</Content>
+        <Footer>Footer</Footer>
+    </Layout>
+
+    <Layout>
+        <Header>Header</Header>
+        <Layout>
+            <Sider hide-trigger>Sider</Sider>
+            <Content>Content</Content>
+        </Layout>
+        <Footer>Footer</Footer>
+    </Layout>
+
+    <Layout>
+        <Header>Header</Header>
+        <Layout>
+            <Content>Content</Content>
+            <Sider hide-trigger>Sider</Sider>
+        </Layout>
+        <Footer>Footer</Footer>
+    </Layout>
+
+    <Layout>
+        <Sider hide-trigger>Sider</Sider>
+        <Layout>
+            <Header>Header</Header>
+            <Content>Content</Content>
+            <Footer>Footer</Footer>
+        </Layout>
+    </Layout>
+    <Button>Default</Button>
+    <Button type="primary">Primary</Button>
+    <Button type="ghost">Ghost</Button>
+    <Button type="dashed">Dashed</Button>
+    <Button type="text">Text</Button>
+    <br><br>
+    <Button type="info">Info</Button>
+    <Button type="success">Success</Button>
+    <Button type="warning">Warning</Button>
+    <Button type="error">Error</Button>
+</div>
+</div>
+</template>
+<script>
+    export default {
+
+    }
+</script>
+
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      // msg: 'Welcome to Your Vue.js App'
+      value: [20,50]
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
